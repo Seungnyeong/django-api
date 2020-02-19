9425aa8cdd517d1b37e6122d99769e9f31aa1888
mellow0410@naver.com <mellow0410@gmail.com>
2020-02-20 00:47:08 +0900

    API view

--- rooms/views.py
+++ rooms/views.py
@@ -1,12 +1,15 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Room
 from .serializers import RoomSerializer
 from django.shortcuts import reverse, redirect
 
-@api_view(["GET"])# Create your views here.
-def list_rooms(request):
+class ListRoomsView(APIView):
 
-    rooms = Room.objects.all()
-    serialized_rooms = RoomSerializer(rooms, many=True)
-    return Response(data=serialized_rooms.data)
+    def get(self, request):
+        rooms = Room.objects.all()
+        serializer = RoomSerializer(rooms, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        pass
